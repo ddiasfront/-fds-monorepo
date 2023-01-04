576cd131abdf83399c75c0f52eb9630ffb32fc3f
Diego Dias <diegodias@Diegos-MacBook-Pro.local>
2023-01-04 10:05:49 +0000

    Removed unused files and added variant to button

--- components/src/components/Button.tsx
+++ components/src/components/Button.tsx
@@ -1,8 +1,18 @@
 import { ButtonCSS } from "@freedom-ds/styles";
 import { styled } from "@freedom-ds/web";
 
-const StyledButton = styled("button", ButtonCSS, styled);
+const StyledButton = styled("button", ButtonCSS);
+const StyledButtonOutlined = styled("button", ButtonCSS);
 
-export function Button() {
-  return <StyledButton>Hello World ! 🌏</StyledButton>;
+enum ButtonVariants {
+  outlined = "outlined",
+}
+
+export type ButtonStyleProps = {
+  variant: ButtonVariants;
+  label: string;
+};
+
+export function Button({ variant, label }: ButtonStyleProps) {
+  return <StyledButton variant={variant}>{label}</StyledButton>;
 }
--- styles/src/Button/ButtonStyles.tsx
+++ styles/src/Button/ButtonStyles.tsx
@@ -4,7 +4,7 @@ export const ButtonCSS = css({
   px: "$3",
   border: "1.25px solid $blue500",
   borderRadius: "$rl",
-  fontSize: "$xl",
+  fontSize: "$sm",
   fontWeight: "$thin",
   fontFamily: "Work Sans",
   backgroundColor: "$blue600",
@@ -12,5 +12,13 @@ export const ButtonCSS = css({
   transition: "all 0.5s ease",
   "&:hover": {
     backgroundColor: "$blue500",
+  },
+  variants: {
+    variant: {
+      outlined: {
+        backgroundColor: "transparent",
+        color: "$blue500",
+      },
+    },
   },
 });
